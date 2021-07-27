54ab0012045baf890e5f9bf589e9b10eeef520a5
EajayD <edelos1524@hotmail.com>
2021-07-26 21:31:23 -0700

    added initial prompt and view all departments function

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 // Dpendencies
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const consoleTable = require('console.table');
+const cTable = require('console.table');
 
 // Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
-      password: '',
+      // Your password here
+      password: 'Kiseki24!',
       database: 'tracker_db'
     },
     console.log(`Connected to the tracker_db database.`)
   );
+
+  questions();
+
+  function questions() {
+      inquirer.prompt(
+          {
+              type: "list",
+              message: "Please select one of the following",
+              name: "choice",
+              choices: [
+                  "View all departments",
+                  "View all roles",
+                  "View all employees",
+                  "Add a department",
+                  "Add a role",
+                  "Add an employee",
+                  "Update an employee role"
+              ]
+          }
+      ).then(answer => {
+          let {choice} = answer;
+          if (choice === 'View all departments') {
+              viewDepartments();
+          }
+      })
+  }
+
+function viewDepartments() {
+    db.promise().query(`SELECT * FROM department`)
+        .then(dept => {
+            console.log('dept', dept[0]);
+            console.table(dept[0]);
+            questions();
+        })
+        .catch(err => {
+            console.log('Error', err);
+        })
+}
